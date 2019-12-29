37b84bb02b9d40ff88ec3d63eccbf17d240675a7
Vladimir Kuzmin <Vladimir_kyzmin@mail.ru>
2019-12-30 00:14:04 +0300

    add code

---
+++ src/test/java/CityList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CityList {
+
+    public static String getRandomCity() {
+        ArrayList<String> city = new ArrayList<>();
+        city.add("Астрахать");
+        city.add("Волгоград");
+        city.add("Грозный");
+        city.add("Махачкала");
+        city.add("Самара");
+        city.add("Ростов-на-Дону");
+        city.add("Краснодар");
+        city.add("Владивосток");
+        city.add("Новосибирск");
+
+        Random random = new Random();
+        return city.get(random.nextInt(city.size()));
+    }
+}
---
+++ src/test/java/WrongPhoneNumList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class WrongPhoneNumList {
+
+    public static String getWrongPhoneNumber() {
+        ArrayList<String> phone = new ArrayList<>();
+        phone.add("+7000123456");
+        phone.add("+");
+        phone.add("00000000000");
+        phone.add("telefon");
+        phone.add("<br>");
+
+        Random random = new Random();
+        return phone.get(random.nextInt(phone.size()));
+    }
+}
